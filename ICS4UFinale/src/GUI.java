5963cdfd415d1b351a0b933b4b2997c527ad7b6d
khadija2003 <79233527+khadija2003@users.noreply.github.com>
2021-06-24 23:29:08 -0400

    Update GUI.java

--- ICS4UFinale/src/GUI.java
+++ ICS4UFinale/src/GUI.java
@@ -1,4 +1,9 @@
+/////////////////////
+//ICS4U PROJET FINALE
+//KHADIJA AISSIOUI
+////////////////////
 
+//GUI ajoute les interfaces graphic 
 public class GUI {
 		public static void main(String[] args){
 		new Menu();
