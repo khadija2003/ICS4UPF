516a6b1574424610eaae8f3b75edf8bcb08356b7
khadija2003 <79233527+khadija2003@users.noreply.github.com>
2021-06-24 23:27:32 -0400

    Update Dificulte.java

--- ICS4UFinale/src/Dificulte.java
+++ ICS4UFinale/src/Dificulte.java
@@ -1,4 +1,7 @@
-
+//////////////////////
+//ICS4U PROJET FINALE
+//KHADIJA AISSIOUI
+/////////////////////
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -6,30 +9,30 @@ import javax.swing.*;
 public class Dificulte {
 
 	public Dificulte() {
-		
+		//ce code definie les difficultes a chaque niveau et te donne l'option de choisir un a partir du menu
 		final JFrame cadre = new JFrame("Menu");	
 		BoxLayout boxLayout = new BoxLayout(cadre.getContentPane(), BoxLayout.Y_AXIS); 
-		
+		//le boutton pour l'option facile
 		Button facile = new Button("facile");
 		facile.setFont(new Font("Verdana", Font.BOLD, 32));
-		
+		//le boutton pour l'option moyen
 		Button moyen = new Button("moyen");
 		moyen.setFont(new Font("Verdana", Font.BOLD, 32));
-		
+		//le boutton pour l'option dur
 		Button dur = new Button("dur");
 		dur.setFont(new Font("Verdana", Font.BOLD, 32));
-		
+		//nommee chaque boutton 
 		cadre.add(facile); 
 		cadre.add(moyen);
 		cadre.add(dur);
-
+		//layout des bouttons
 		cadre.setLayout(boxLayout);
 		cadre.setSize(500,500);
 		cadre.setLocationRelativeTo(null);
 		cadre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		cadre.setResizable(false);
 		cadre.setVisible(true);		
-		
+		//les characteristique du niveau facile
 		facile.addActionListener(new ActionListener()
 		{
 			public void actionPerformed(ActionEvent ae)
@@ -38,7 +41,7 @@ public class Dificulte {
 				new Cadre(10);
 			}
 		});
-		
+		//les characteristique du niveau moyen
 		moyen.addActionListener(new ActionListener()
 		{
 			public void actionPerformed(ActionEvent ae)
@@ -48,7 +51,7 @@ public class Dificulte {
 				
 			}
 		});
-	
+		//les characteristique du niveau dur
 		dur.addActionListener(new ActionListener()
 		{
 			public void actionPerformed(ActionEvent ae)
